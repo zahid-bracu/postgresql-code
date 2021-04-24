edf9cdaf80e0a457c6c55ec9c8f5b0bf2f26a376
zahid-bracu <zahid.mym@gmail.com>
2021-04-24 21:11:07 +0600

    ok

--- posgresql.sql
+++ posgresql.sql
@@ -1,10 +1,14 @@
-\? ⇒ information
-\help ⇒ help information
-\l ⇒ list of database
-\q ⇒ quit db
-Create database db_name;
-\c db_name; ⇒ use db
-Drop database db_name;
-Drop table table_name;
-\d ⇒ check table exists or not
-\d table_name ⇒ check table details
+--information
+\?
+--information for help
+\help
+--list of database
+\list
+--quit database
+\quit
+--use database
+\c yelp
+--check tables
+\d
+--check table details
+\d table_name
