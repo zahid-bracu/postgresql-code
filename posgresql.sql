6ad54fae7765f5a29f4c3fe969383f9a116846ea
zahid-bracu <zahid.mym@gmail.com>
2021-04-25 22:10:38 +0600

    ok

--- posgresql.sql
+++ posgresql.sql
@@ -12,3 +12,25 @@ help
 \d
 --check table details
 \d table_name
+
+
+--create table
+CREATE TABLE COMPANY(
+   ID  SERIAL PRIMARY KEY,
+   NAME           TEXT      NOT NULL,
+   AGE            INT       NOT NULL,
+   ADDRESS        CHAR(50),
+   SALARY         REAL
+);
+
+
+
+\i C:/Users/zahid/Downloads/mock_data.sql
+
+
+--select : result total row number
+SELECT FROM company ; 
+
+
+---offset & limit
+SELECT * FROM company OFFSET 10 LIMIT 5;
